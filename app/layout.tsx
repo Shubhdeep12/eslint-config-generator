ee886a0f45584d100e2c414c456ce5c49317a9f6
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-05-27 08:30:33 +0530

    added assets and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,71 @@ import MainContent from "@/components/MainContent";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ESlint Generator",
-  description: "Easily generate eslint for your app.",
+  title: "ESLint Config Generator",
+  description: "Easily config your eslint config for your next app.",
+  applicationName: "ESLint Config Generator",
+  authors: [
+    { name: "Shubhdeep Chhabra", url: "https://www.shubhdeepchhabra.in" },
+  ],
+  keywords: [
+    "create eslint config in next.js",
+    "create eslint config in react",
+    "react",
+    "next.js",
+    "javascript",
+    "ecmascript",
+    "commonjs",
+    "eslint",
+    "linter",
+    "linter in javascript app",
+  ],
+  referrer: "origin",
+  creator: "Shubhdeep Chhabra",
+  publisher: "Shubhdeep Chhabra",
+  openGraph: {
+    type: "website",
+    description: "Easily config your eslint config for your next app.",
+    title: "ESLint Config Generator",
+    locale: "en_US",
+    siteName: "ESLint Config Generator",
+    url: "https://shubhdeep12.github.io/eslint-config-generator/",
+    // images: [
+    // 	{
+    // 		url: '/assets/Shubhdeepchhabra.png',
+    // 		alt: 'Shubhdeep Chhabra',
+    // 		width: '1200',
+    // 		height: '630',
+    // 	},
+    // ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@ShubhInTech",
+    creator: "@ShubhInTech",
+    title: "ESLint Config Generator",
+    description: "Easily config your eslint config for your next app.",
+    // images: ['/assets/Shubhdeepchhabra.png'],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    other: {
+      rel: "apple-touch-icon-precomposed",
+      url: "/apple-touch-icon.png",
+    },
+  },
 };
 
 export default function RootLayout({
