87d271a812d45ce374f80803f269556c6d104062
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-06-03 16:36:47 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/eslint-config-generator",
-  output: "export",
-  reactStrictMode: false,
 };
 
 export default nextConfig;
